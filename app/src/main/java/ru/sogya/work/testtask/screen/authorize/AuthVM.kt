1cc26a94d323e92135bbc5032b012c07e477708f
SogYa <Ghost.Spartan36@gmail.com>
2023-09-06 12:09:01 +0300

    added vm realizations

--- app/src/main/java/ru/sogya/work/testtask/screen/authorize/AuthVM.kt
+++ app/src/main/java/ru/sogya/work/testtask/screen/authorize/AuthVM.kt
@@ -1,8 +1,24 @@
 package ru.sogya.work.testtask.screen.authorize
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import ru.sogya.work.testtask.domain.usecase.InsertClientInfoUseCase
+import ru.sogya.work.testtask.mapper.ClientInfoPresentationMapper
+import ru.sogya.work.testtask.mdoel.ClientInfoPresentation
+import javax.inject.Inject
 
 @HiltViewModel
-class AuthVM: ViewModel() {
+class AuthVM @Inject constructor(
+    private val insertClientInfoUseCase: InsertClientInfoUseCase
+) : ViewModel() {
+
+    fun insertClientInfoToDatabase(id: Int, code: Int, name: String, surname: String) {
+        viewModelScope.launch {
+            val clientInfoPresentation = ClientInfoPresentation(id, code, name, surname)
+            val clientInfoData = ClientInfoPresentationMapper(clientInfoPresentation).toData()
+            insertClientInfoUseCase(clientInfoData)
+        }
+    }
 }
--- app/src/main/java/ru/sogya/work/testtask/screen/home/HomeVM.kt
+++ app/src/main/java/ru/sogya/work/testtask/screen/home/HomeVM.kt
@@ -1,8 +1,35 @@
 package ru.sogya.work.testtask.screen.home
 
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
+import ru.sogya.work.testtask.domain.usecase.GetClientInfoUseCase
+import ru.sogya.work.testtask.mapper.ClientInfoDomainMapper
+import ru.sogya.work.testtask.mdoel.ClientInfoPresentation
+import javax.inject.Inject
 
 @HiltViewModel
-class HomeVM: ViewModel() {
+class HomeVM @Inject constructor(
+    getClientInfoUseCase: GetClientInfoUseCase
+) : ViewModel() {
+    private val clientInfoLiveData = MutableLiveData<ClientInfoPresentation>()
+
+    init {
+        viewModelScope.launch {
+            getClientInfoUseCase().flowOn(Dispatchers.IO).catch {
+                Log.d("GetClientError", it.message.toString())
+            }.map {
+                ClientInfoDomainMapper(it).toPresentation()
+            }.collect{
+                clientInfoLiveData.postValue(it)
+            }
+        }
+    }
 }
