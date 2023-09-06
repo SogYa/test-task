2f007f8e138e78512299d2a04e5bc5409a581756
SogYa <Ghost.Spartan36@gmail.com>
2023-09-06 12:19:55 +0300

    added view binding realization

--- app/src/main/java/ru/sogya/work/testtask/screen/authorize/AuthFragment.kt
+++ app/src/main/java/ru/sogya/work/testtask/screen/authorize/AuthFragment.kt
@@ -1,8 +1,31 @@
 package ru.sogya.work.testtask.screen.authorize
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
+import ru.sogya.work.testtask.databinding.FragmentAuthBinding
 
 @AndroidEntryPoint
 class AuthFragment : Fragment() {
+    private val vm: AuthVM by viewModels()
+    private var _binding: FragmentAuthBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentAuthBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding=null
+    }
 }
--- app/src/main/java/ru/sogya/work/testtask/screen/home/HomeFragment.kt
+++ app/src/main/java/ru/sogya/work/testtask/screen/home/HomeFragment.kt
@@ -1,8 +1,31 @@
 package ru.sogya.work.testtask.screen.home
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
+import ru.sogya.work.testtask.databinding.FragmentHomeBinding
 
 @AndroidEntryPoint
-class HomeFragment:Fragment() {
+class HomeFragment : Fragment() {
+    private val vm: HomeVM by viewModels()
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 }
