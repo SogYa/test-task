ca6192f369b5c568d501d13c515cf825a508aa60
SogYa <Ghost.Spartan36@gmail.com>
2023-09-06 11:40:29 +0300

    added client-info data model

---
+++ data/src/main/java/ru/sogya/work/testtask/data/model/ClientInfoData.kt
@@ -0,0 +1,14 @@
+package ru.sogya.work.testtask.data.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import ru.sogya.work.testtask.domain.models.ClientInfoDomain
+
+@Entity(tableName = "client_info")
+data class ClientInfoData(
+    @PrimaryKey
+    override val id: Int,
+    override val code: Int,
+    override val name: String,
+    override val surname: String
+) : ClientInfoDomain
