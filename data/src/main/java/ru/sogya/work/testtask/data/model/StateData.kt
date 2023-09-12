5c695507aae607c0b718e8005b6511092bea7184
SogYa <Ghost.Spartan36@gmail.com>
2023-09-12 11:25:10 +0300

    added serialized name

--- data/src/main/java/ru/sogya/work/testtask/data/model/StateData.kt
+++ data/src/main/java/ru/sogya/work/testtask/data/model/StateData.kt
@@ -1,5 +1,10 @@
 package ru.sogya.work.testtask.data.model
 
+import com.google.gson.annotations.SerializedName
 import ru.sogya.work.testtask.domain.models.StateDomain
 
-data class StateData(override val date: String, override val names: List<String>) : StateDomain
+data class StateData(
+    @SerializedName("date")
+    override val date: String,
+    @SerializedName("name")
+    override val names: List<String>) : StateDomain
