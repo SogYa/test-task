ad7742589a236406a8d4cd80972c770fc0503430
SogYa <Ghost.Spartan36@gmail.com>
2023-09-12 11:25:19 +0300

    changed return type

--- data/src/main/java/ru/sogya/work/testtask/data/network/api/NetworkApi.kt
+++ data/src/main/java/ru/sogya/work/testtask/data/network/api/NetworkApi.kt
@@ -1,10 +1,10 @@
 package ru.sogya.work.testtask.data.network.api
 
 import retrofit2.http.GET
-import ru.sogya.work.testtask.data.model.StateData
+import ru.sogya.work.testtask.data.model.StateListData
 
 interface NetworkApi {
 
     @GET("/data")
-    suspend fun getAllStates(): List<StateData>
+    suspend fun getAllStates(): StateListData
 }
