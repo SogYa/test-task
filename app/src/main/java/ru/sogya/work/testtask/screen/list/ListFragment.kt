4d356755636e4129b4a50b2ff916f2dd3cd74145
SogYa <Ghost.Spartan36@gmail.com>
2023-09-12 11:04:38 +0300

    added basic realization

--- app/src/main/java/ru/sogya/work/testtask/screen/list/ListFragment.kt
+++ app/src/main/java/ru/sogya/work/testtask/screen/list/ListFragment.kt
@@ -1,10 +1,44 @@
 package ru.sogya.work.testtask.screen.list
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
+import ru.sogya.work.testtask.R
+import ru.sogya.work.testtask.databinding.FragmentListBinding
 
 @AndroidEntryPoint
-class ListFragment: Fragment() {
-    private val vm:ListVM by viewModels()
+class ListFragment : Fragment(R.layout.fragment_list) {
+    private var _binding: FragmentListBinding? = null
+    private val binding get() = _binding!!
+    private val vm: ListVM by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        vm.getStateLiveData().observe(viewLifecycleOwner){
+            
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 }
--- app/src/main/java/ru/sogya/work/testtask/screen/list/ListVM.kt
+++ app/src/main/java/ru/sogya/work/testtask/screen/list/ListVM.kt
@@ -1,8 +1,37 @@
 package ru.sogya.work.testtask.screen.list
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
+import ru.sogya.work.testtask.domain.usecase.states.GetAllStatesUseCase
+import ru.sogya.work.testtask.mapper.StateListDomainMapper
+import ru.sogya.work.testtask.model.StatePresentation
+import javax.inject.Inject
 
 @HiltViewModel
-class ListVM : ViewModel() {
+class ListVM @Inject constructor(getAllStatesUseCase: GetAllStatesUseCase) :
+    ViewModel() {
+    private val stateLiveData = MutableLiveData<List<StatePresentation>>()
+
+    init {
+        viewModelScope.launch {
+            getAllStatesUseCase().flowOn(Dispatchers.IO).catch {
+                Log.d("StateListError", it.message.toString())
+            }.map {
+                StateListDomainMapper(it).toPresentationList()
+            }.collect {
+                stateLiveData.postValue(it)
+            }
+        }
+    }
+
+    fun getStateLiveData(): LiveData<List<StatePresentation>> = stateLiveData
 }
