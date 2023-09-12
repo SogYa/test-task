e59eb7719f716300c587aa57e4b7501cb605df73
SogYa <Ghost.Spartan36@gmail.com>
2023-09-12 10:51:09 +0300

    renamed

---
+++ data/src/main/java/ru/sogya/work/testtask/data/repository/StatesRepositoryImpl.kt
@@ -0,0 +1,14 @@
+package ru.sogya.work.testtask.data.repository
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import ru.sogya.work.testtask.data.network.NetworkService
+import ru.sogya.work.testtask.domain.models.StateDomain
+import ru.sogya.work.testtask.domain.repository.StatesRepository
+
+class StatesRepositoryImpl : StatesRepository {
+    private val apiService = NetworkService.apiService
+    override fun getAllStates(): Flow<List<StateDomain>> = flow {
+        emit(apiService.getAllStates())
+    }
+}
