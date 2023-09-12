3a1c539f7789eea8632ab947fc8b89ea22008002
SogYa <Ghost.Spartan36@gmail.com>
2023-09-12 10:39:18 +0300

    added retrofit and OkHttp dependencies

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -41,6 +41,16 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
+    //Retrofit 2
+    val retrofitVersion = "2.9.0"
+    implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
+    implementation("com.squareup.retrofit2:converter-scalars:$retrofitVersion")
+    implementation("com.squareup.retrofit2:converter-gson:$retrofitVersion")
+    //OkHttp
+    val okHttpVersion = "5.0.0-alpha.11"
+    implementation("com.squareup.okhttp3:okhttp:$okHttpVersion")
+    implementation("com.squareup.okhttp3:logging-interceptor:$okHttpVersion")
+
     //Room
     val roomVersion = "2.5.2"
     implementation("androidx.room:room-runtime:$roomVersion")
